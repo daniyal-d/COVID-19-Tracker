87d4ce1dfa4999fe00cf580f3ef29e4884b871a3
Daniyal <31736868+Dawood700@users.noreply.github.com>
2020-05-20 11:54:03 -0600

    Update Tracker.py

--- Tracker.py
+++ Tracker.py
@@ -18,38 +18,38 @@ class Tracker:
         self.stats = self.data()
 
     def data(self):
-        case = requests.get(f"https://www.parsehub.com/api/v2/projects/{p_token}/last_ready_run/data", params={"api_key": key})
-        stats = json.loads(case.text)
+        case = requests.get(f"https://www.parsehub.com/api/v2/projects/{p_token}/last_ready_run/data", params={"api_key": key}) # Use web scraping to find data
+        stats = json.loads(case.text) # Use the JSON format of the data
         return stats
 
     def worldwide_cases(self):
-        world = self.stats["world_total"]
+        world = self.stats["world_total"] # Find total worldwide cases
         for data in world:
             if data["name"] == "Coronavirus Cases:":
                 return data["world_cases"]
 
     def worldwide_deaths(self):
-        world = self.stats["world_total"]
+        world = self.stats["world_total"] # Find total worldwide deaths
         for data in world:
             if data["name"] == "Deaths:":
                 return data["world_cases"]
 
     def worldwide_recovered(self):
-        world = self.stats["world_total"]
+        world = self.stats["world_total"] # Find total worldwide recoveries
         for data in world:
             if data["name"] == "Recovered:":
                 return data["world_cases"]
 
     def states(self):
-        country = self.stats["states"]
-        state = input("Which countries data do you want to view?: ")
+        country = self.stats["states"] # Find info for a specific country
+        state = input("Which countries data do you want to view?: ") # Ask which countries data a user wishes to view
         for data in country:
             if data["name"].lower() == state.lower():
                 return (f"Info for {state} \n Cases: {data['states_cases']} \n Deaths: {data['states_deaths']} \n Recoveries: {data['states_recovered']}")
 
     def new_info(self, threading=None):
-        hello = requests.post(f'https://www.parsehub.com/api/v2/projects/{p_token}/run', params={"api_key": key})
-        stats = json.loads(hello.text)
+        hello = requests.post(f'https://www.parsehub.com/api/v2/projects/{p_token}/run', params={"api_key": key}) # Updates data
+        stats = json.loads(hello.text) # JSON form of updated data
 
         def update():
             time.sleep(0.1)
@@ -57,7 +57,7 @@ class Tracker:
             while True:
                 new_info = stats()
                 if new_info != old_info:
-                    self.stats = new_info
+                    self.stats = new_info # Convert old data into new data
                     break
                 time.sleep(0.1)
         data.new_info()
